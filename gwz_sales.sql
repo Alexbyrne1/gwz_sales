1611b9f9923f0ad4b386f7c974f8909d0ce8c320
Alexbyrne1 <alexbyrne048@gmail.com>
2024-07-22 15:33:47 +0200

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,5 @@
 SELECT date_date,round(sum(turnover),1)total_turnover
-,round(sum(purchase_cost),1) as total_purchase_cost
+,round(sum(purchase_costes),1) as total_purchase_costes
  FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
  GROUP BY date_date
- Order by date_date
+ Order by date_date 
